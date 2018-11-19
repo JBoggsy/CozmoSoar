3b24d104e9a94512bd7c3bbcfcf6b7e9e68689f0
James Boggs <jmsboggs@gmail.com>
2018-11-19 13:18:04 -0500

    Create initial CozmoSoar object code.
    I added a stub for the CozmoSoar class, adding in only an init method, which
    is really only the barebones.

---
+++ cozmo.py
@@ -0,0 +1,22 @@
+import soar.Python_sml_ClientInterface as sml
+import cozmo
+from cozmo.camera import Camera
+from cozmo.util import degrees, distance_mm, speed_mmps
+
+class CozmoSoar(object):
+    """
+    A class representing the Soar interface with a Cozmo robot.
+
+    A `CozmoSoar` object holds a reference to two critical things: a `cozmo.Robot` instance used
+    to talk with the actual Cozmo robot, and a `sml.Agent` instance used to communicate with the
+    Soar kernel.
+    """
+    def __init__(self, robot, agent):
+        """
+        Create a new `CozmoState` instance with the given robot and agent.
+
+        :param robot: The Cozmo robot to interface with
+        :param agent: The SML agent in Soar to communicate with
+        """
+        self.robot = robot
+        self.agent = agent
--- main.py
+++
@@ -1,4 +0,0 @@
-import soar.Python_sml_ClientInterface as sml
-import cozmo
-from cozmo.camera import Camera
-from cozmo.util import degrees, distance_mm, speed_mmps
