28711b6d90516f9dcf013d5c3aca5e03679726b6
Aaron Mininger <mininger@umich.edu>
2019-07-19 10:23:22 -0400

    Added interface for adding camera localization code

---
+++ CameraLocalizer.py
@@ -0,0 +1,24 @@
+
+
+class CameraLocalizer:
+
+
+    def __init__(self):
+        # Spin up a thread to periodically update the transfor
+
+        # transform
+    
+    # pose: xyzrpy
+    def update_robot_pose(self, pose):
+        pass
+
+    # cozmo_pose: xyzrpy
+    def get_world_pose(self, cozmo_pose):
+        pass
+
+    # world_pose: xyzrpy
+    def get_cozmo_pose(self, world_pose):
+        pass
+
+
+
